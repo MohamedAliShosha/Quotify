7b2a749cdcbc9838f3b42b2218d257efc251ee08
Mohamed Ali <a74498722@gmail.com>
2025-07-12 08:48:30 -0700

    adding colors manager file for colors managing

---
+++ lib/core/utils/colors_manager.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+abstract class ColorsManager {
+  static const kPrimaryColor = Color(0xffFAF9F6);
+  static const kBlackColor = Colors.black;
+  static const kWhiteColor = Colors.white;
+}
