2c7803a101f73eaf555b107d6da6a4540f24cc74
Mohamed Ali <a74498722@gmail.com>
2025-07-12 08:52:09 -0700

    `Added OnBoardingView route to AppRouter and updated MaterialApp to use routerConfig`

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,4 +1,5 @@
 import 'package:go_router/go_router.dart';
+import 'package:starter_template/features/OnBoarding/presentation/views/on_boarding_view.dart';
 
 abstract class AppRouter {
   // The first thing when creating routers is declaring routes variables
@@ -10,6 +11,13 @@ abstract class AppRouter {
 
   // Then creating the router variable that = GoRouter"Contains a list of routes"
   static final router = GoRouter(
-    routes: [],
+    routes: [
+      GoRoute(
+        path: '/',
+        builder: (context, state) {
+          return const OnBoardingView();
+        },
+      )
+    ],
   );
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:starter_template/core/utils/app_router.dart';
 
 class QuotesApp extends StatelessWidget {
   const QuotesApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(),
+    return MaterialApp.router(
+      routerConfig: AppRouter.router,
+      debugShowCheckedModeBanner: false,
     );
   }
 }
