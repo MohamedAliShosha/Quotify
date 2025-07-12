3bfe0f57e941d10e16d7df09c59dcef5a6da317e
Mohamed Ali <a74498722@gmail.com>
2025-07-12 09:52:59 -0700

    "Removed lib/core/functions/.gitkeep file" and separate build border methods

---
+++ lib/core/functions/build_enabled_border.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+OutlineInputBorder buildEnabledBorder({required Color color}) {
+  return OutlineInputBorder(
+    borderSide: BorderSide(
+      color: color,
+    ),
+  );
+}
---
+++ lib/core/widgets/custom_email_and_password_text_form_field.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:starter_template/core/functions/build_enabled_border.dart';
+import 'package:starter_template/core/functions/build_focused_border.dart';
+import 'package:starter_template/core/utils/colors_manager.dart';
+
+// ignore: must_be_immutable
+class CustomEmailAndPasswordTextFormField extends StatelessWidget {
+  CustomEmailAndPasswordTextFormField(
+      {super.key,
+      this.onChanged,
+      this.hintText,
+      this.obscuredText = false,
+      required this.labelText});
+
+  final String labelText;
+  Function(String)? onChanged;
+  String? hintText;
+  bool? obscuredText; // I gave  it a default value of false
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 10, right: 10),
+      child: TextFormField(
+        cursorColor: Colors.black,
+        obscureText: obscuredText!,
+        // Text form field contains validator that used to validate inputs
+        validator: (data) {
+          if (data!.isEmpty) {
+            return 'this field is required';
+          } else {
+            return null;
+          }
+        },
+        onChanged: onChanged,
+        decoration: InputDecoration(
+          labelText: labelText,
+          labelStyle: const TextStyle(fontSize: 16, color: Colors.grey),
+          hintText: hintText,
+          hintStyle: const TextStyle(
+            color: ColorsManager.kBlackColor,
+          ),
+          enabledBorder: buildEnabledBorder(color: ColorsManager.kBlackColor),
+          focusedBorder: buildFocusedBorder(color: ColorsManager.kBlackColor),
+        ),
+      ),
+    );
+  }
+}
