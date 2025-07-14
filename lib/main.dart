2f33d593c9816552178271482e92363f92c29bad
Mohamed Ali <a74498722@gmail.com>
2025-07-14 11:51:46 -0700

    feat: initialize Firebase services in main
    Add Firebase Core and App Check initialization to ensure Firebase services are ready before app startup. This enables Firebase functionality throughout the application.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_app_check/firebase_app_check.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:starter_template/core/utils/app_router.dart';
 
@@ -13,6 +15,12 @@ class QuotesApp extends StatelessWidget {
   }
 }
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  await FirebaseAppCheck.instance.activate(
+    androidProvider: AndroidProvider.debug,
+    
+  );
   runApp(const QuotesApp());
 }
