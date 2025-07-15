27e2b6fff9a570bbeeb6dbd5510cc79b61fdaf3a
Mohamed Ali <a74498722@gmail.com>
2025-07-14 19:44:57 -0700

    refactor: move firebase initialization to app_initializers
    Extract firebase initialization logic into a dedicated utility class to improve code organization and reusability

---
+++ lib/core/utils/app_initializers.dart
@@ -0,0 +1,12 @@
+import 'package:firebase_app_check/firebase_app_check.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+abstract class AppInitializers
+{
+  static Future<void> initializeFirebase() async {
+    await Firebase.initializeApp();
+    await FirebaseAppCheck.instance.activate(
+      androidProvider: AndroidProvider.debug,
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-import 'package:firebase_app_check/firebase_app_check.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:starter_template/core/utils/app_initializers.dart';
 import 'package:starter_template/core/utils/app_router.dart';
 import 'package:starter_template/core/utils/colors_manager.dart';
 
@@ -22,10 +21,6 @@ class QuotesApp extends StatelessWidget {
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  await FirebaseAppCheck.instance.activate(
-    androidProvider: AndroidProvider.debug,
-    
-  );
+  await AppInitializers.initializeFirebase();
   runApp(const QuotesApp());
 }
